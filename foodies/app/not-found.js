793bd629b3ed7c95dd478ae60d58859bd49d01b9
krsachin9696 <krsachin9696@gmail.com>
2024-09-24 14:55:49 +0530

    error not found

---
+++ foodies/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Failed to fetch meal data. Please try again Later.</p>
+    </main>
+  )
+}
